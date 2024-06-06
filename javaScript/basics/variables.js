4e70ee7e5993e06ce8b7827797e1d75255c6d651
Rahul Kumar <128039140+rahuljaiswaal@users.noreply.github.com>
2024-06-06 11:01:59 +0530

    chai code js

---
+++ javaScript/basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 143
+let accountEmail = "rahul@18"
+var accountPassword = "180745"
+accountCity = "motihari"
+// accountId = 2    --> changing in constant not possible
+
+accountEmail = "vk@12"
+accountPassword = "789"
+accountCity = "goa"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ javaScript/hello.js
@@ -0,0 +1,2 @@
+console.log("rahul")
+console.log("wow")
